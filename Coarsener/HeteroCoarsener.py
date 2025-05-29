1dee9c2e99e562a3e3a01949fb0cbe40372f6d1e
Robino-CK <robin.kollmann@yahoo.de>
2025-05-29 22:48:45 +0200

    bug fixing

--- Coarsener/HeteroCoarsener.py
+++ Coarsener/HeteroCoarsener.py
@@ -6,12 +6,13 @@ import time
 
 
 class HeteroCoarsener(ABC):
-    def __init__(self, graph: dgl.DGLHeteroGraph, r:float, device="cpu"):
+    def __init__(self, graph: dgl.DGLHeteroGraph, r:float, num_nearest_init_neighbors_per_type, device="cpu"):
         self.original_graph = graph
         self.summarized_graph = deepcopy(graph)
         self.summarized_graph = self.summarized_graph.to(device)
         self.r = r
         self.device = device
+        self.num_nearest_init_neighbors_per_type = num_nearest_init_neighbors_per_type
         pass
     
     @abstractmethod
@@ -30,10 +31,10 @@ class HeteroCoarsener(ABC):
     def _init_costs(self):
         def _compute_embeddings(node_type, etype=None, use_feat=True):
             # Select the proper feature tensor
-            if use_feat and 'feat' in self.coarsened_graph.nodes[node_type].data:
-                H = self.coarsened_graph.nodes[node_type].data['feat'].float()
-            elif etype is not None and f'h{etype}' in self.coarsened_graph.nodes[node_type].data:
-                H = self.coarsened_graph.nodes[node_type].data[f'h{etype}'].float()
+            if use_feat and 'feat' in self.summarized_graph.nodes[node_type].data:
+                H = self.summarized_graph.nodes[node_type].data['feat'].float()
+            elif etype is not None and f'h{etype}' in self.summarized_graph.nodes[node_type].data:
+                H = self.summarized_graph.nodes[node_type].data[f'h{etype}'].float()
             else:
                 # TODO this case should not happen
                 pass
@@ -45,7 +46,7 @@ class HeteroCoarsener(ABC):
             # compute pairwise L1 distances
             dist_matrix = torch.cdist(H, H, p=1)
             # get k smallest distances (including self)
-            k = min(self.num_nearest_per_etype, H.size(0))
+            k = min(self.num_nearest_init_neighbors_per_type[etype], H.size(0))
             dists, idxs = torch.topk(dist_matrix, k, largest=False)
             return dists, idxs
 
@@ -60,11 +61,11 @@ class HeteroCoarsener(ABC):
         
         start_time = time.time()
         init_costs = dict()
-        for src_type, etype, dst_type in self.coarsened_graph.canonical_etypes:
+        for src_type, etype, dst_type in self.summarized_graph.canonical_etypes:
            init_costs[etype] = _init_calculate_clostest_edges(src_type, etype, dst_type)
           
-        for ntype in self.coarsened_graph.ntypes:
-            if "feat" in self.coarsened_graph.nodes[dst_type].data:
+        for ntype in self.summarized_graph.ntypes:
+            if "feat" in self.summarized_graph.nodes[dst_type].data:
                 init_costs[ntype] = _init_calculate_clostest_features(ntype)   
         
         print("_init_costs", time.time() - start_time)
@@ -72,15 +73,15 @@ class HeteroCoarsener(ABC):
     
     def _get_union(self, init_costs):
         start_time = time.time()
-        closest = {src: {} for src,_,_ in self.coarsened_graph.canonical_etypes}
+        closest = {src: {} for src,_,_ in self.summarized_graph.canonical_etypes}
         # edges
-        for src, etype, _ in self.coarsened_graph.canonical_etypes:
+        for src, etype, _ in self.summarized_graph.canonical_etypes:
             dists, idxs = init_costs[etype]
             for i, neighbors in enumerate(idxs):
                 neigh = set(neighbors.tolist()) - {i}
                 closest[src].setdefault(i, set()).update(neigh)
         # features
-        for ntype in self.coarsened_graph.ntypes:
+        for ntype in self.summarized_graph.ntypes:
             if not ntype in init_costs.keys():
                 continue 
             dists, idxs = init_costs[ntype]
--- init.py
+++ init.py
@@ -4,5 +4,6 @@ from Data.test_graphs import TestHetero, TestHomo
 
 if __name__ == "__main__":
     test = TestHomo()
-    coarsener = HeteroRGCNCoarsener(test.g, 0.4)
+    num_nearest_init_neighbors_per_type = {"follows": 10, "user": 10}
+    coarsener = HeteroRGCNCoarsener(test.g, 0.4, num_nearest_init_neighbors_per_type)
     coarsener.init()
